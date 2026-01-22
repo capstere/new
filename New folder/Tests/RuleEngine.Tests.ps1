92ee740fc48322357d791cedd4d0c27fe07abe26
capstere <jesper.fredriksson2@gmail.com>
2026-01-22 19:37:52 +0100

    Delete New folder directory

--- New folder/Tests/RuleEngine.Tests.ps1
+++
@@ -1,7 +0,0 @@
-﻿﻿#requires -Version 5.1
-# Kör: Invoke-Pester -Path .\Tests
-Describe "RuleEngine smoke tests" {
-    It "Can dot-source RuleEngine without errors" {
-        { . "$PSScriptRoot\..\Modules\RuleEngine.ps1" } | Should Not Throw
-    }
-}
